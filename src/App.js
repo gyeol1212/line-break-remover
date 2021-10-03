afdff5372d5cfa59b2394a17792ac990c6ff945b
hangyeol.shin <hangyeol.shin@navercorp.com>
2021-10-04 03:18:27 +0900

    :art: init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import {useState} from "react";
 import './App.css';
 
 function App() {
-  return (
+  const [value, setValue] = useState('')
+
+  function handleChange(e) {
+    setValue(e.target.value)
+  }
+
+  function removeLineBreak(v) {
+    let target = v;
+    target = target.split('\n').join(' ')
+    target = target.split('- ').join('')
+
+    return target
+  }
+
+
+    function handleCopy() {
+      const target = removeLineBreak(value)
+
+      // textarea 생성
+      const textArea = document.createElement('textarea');
+      // textarea 추가
+      document.body.appendChild(textArea);
+      // textara의 value값으로 div내부 텍스트값 설정
+      textArea.value = target;
+      // textarea 선택 및 복사
+      textArea.select(); document.execCommand('copy');
+      // textarea 제거
+      document.body.removeChild(textArea);
+    }
+
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p className='title'>변환 대상</p>
+      <textarea className='input' value={value} onChange={handleChange} placeholder='텍스트를 입력해주세요.' />
+      <p className='title'>
+          변환 결과<span>텍스트를 클릭하면 복사할 수 있습니다.</span>
+      </p>
+      <div className='result' onClick={handleCopy}>{removeLineBreak(value)}</div>
     </div>
   );
 }
